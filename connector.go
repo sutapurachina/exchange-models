f079b8de2916d5bac758fe78ab988578b40b9b17
sutapurachina <suta.purachina@yahoo.com>
2024-06-20 18:36:36 +0400

    new version of azbit

--- connector.go
+++ connector.go
@@ -8,4 +8,5 @@ type Connector interface {
 	OrderBook(base, quote string, side Side, basePrecision, pricePrecision int, offset, limit int64) ([]*NetOrder, error)
 	FullOrderBook(base, quote string, side Side, basePrecision, pricePrecision int) ([]*NetOrder, error)
 	BestBidBestAsk(base, quote string) (bestBid, bestAsk float64, err error)
+	LastPrice(base, quote string) (lastPrice float64, err error)
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/sutapurachina/azbit-go-sdk v0.0.0-20240617145245-6bd8adc67b33 // indirect
+	github.com/sutapurachina/azbit-go-sdk v0.0.0-20240620131257-170de28fd70a // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
